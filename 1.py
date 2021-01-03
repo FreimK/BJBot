d172dcfeb24d7848bdca4596000c5abe30f8af20
Scottish Wombat <51965560+hadder10@users.noreply.github.com>
2021-01-03 20:44:30 +0300

    Жду блять правильный токен

---
+++ 1.py
@@ -0,0 +1,56 @@
+import os
+import discord
+
+token = 'Gtt_GKhwF9Q1TXUtZ9gWcAPg_HAeBYMo'
+
+id = 795336952022958130
+
+roli = {
+    '♂️': 756976295681720500,
+    '🗑️': 773173503829868555,
+    '⛓️': 756976920268112024
+}
+count = ()
+
+
+class Main(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_raw_reaction_add(self, payload):
+        if payload.message_id == id:
+            channel = self.get_channel(payload.channel_id) # получаем объект канала
+            message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
+            member = utils.get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
+
+            try:
+                emoji = str(payload.emoji) # эмоджик который выбрал чувак
+                role = utils.get(message.guild.roles, id=roliS[emoji]) # объект выбранной роли
+                await member.add_roles(role) # человек получает роль
+                print('{0.display_name} была выдана роль {1.name} в нашем gym'.format(member, role))
+
+            except KeyError as e:
+                print('Не найдена роль для данного эмодзи'+ emoji)
+            except Exception as e:
+                print(repr(e))
+
+    async def on_raw_reaction_remove(self, payload):
+        channel = self.get_channel(payload.channel_id) # получаем объект канала
+        message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
+        member = utils.get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
+
+        try:
+            emoji = str(payload.emoji) # эмоджик который выбрал чувак
+            role = utils.get(message.guild.roles, id=roli[emoji]) # объект выбранной роли
+
+            await member.remove_roles(role)
+            print('Billy забрал у {0.display_name} роль {1.name}'.format(member, role))
+
+        except KeyError as e:
+            print('Не найдена роль для данного эмодзи' + emoji)
+        except Exception as e:
+            print(repr(e))
+
+
+client = Main()
+client.run(token)
