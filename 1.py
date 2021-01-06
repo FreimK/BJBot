de507f2d2366360633b63d8c8e925a11d6a99d66
Scottish Wombat <51965560+hadder10@users.noreply.github.com>
2021-01-06 09:28:59 +0300

    Пришел, доделал, победил.

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
 import discord
 from discord import utils
-from convars import TOKEN
+#from convars import TOKEN
 
+token = ' Nzk1MzI5NDMxNDYzMjY0MjU2.X_HyQA.LHAFiAYmjzmmm9Re0FEoATzw5ZI'
 
 ROLES = {
     '♂️': 756976295681720500,  # Instructor
@@ -21,16 +22,20 @@ class Main(discord.Client):
 
     async def on_raw_reaction_add(self, payload):
         member = payload.member
-        try:
-            emoji = str(payload.emoji)  # эмоджик который выбрал чувак
-            role = utils.get(self.main_guild.roles, id=ROLES[emoji])  # объект выбранной роли
-            await member.add_roles(role)  # человек получает роль
-            print('{0.display_name} была выдана роль {1.name} в нашем gym'.format(member, role))
-        except KeyError:
-            print('Не найдена роль для данного эмодзи ' + emoji)
-        except Exception as e:
-            print(repr(e))
+        if payload.message_id == 795336952022958130:
+            try:
+                emoji = str(payload.emoji)  # эмоджик который выбрал чувак
+                role = utils.get(self.main_guild.roles, id=ROLES[emoji])  # объект выбранной роли
+                channel = self.get_channel(payload.channel_id) # объект канала 
+                message = await channel.fetch_message(payload.message_id) # берется объект сообщения
+                await member.add_roles(role)  # человек получает роль
+                print('{0.display_name} была выдана роль {1.name} в нашем gym'.format(member, role))
+                await message.remove_reaction(emoji, member) # удаляется реакция
+            except KeyError:
+                print('Не найдена роль для данного эмодзи ' + emoji)
+            except Exception as e:
+                print(repr(e))
 
 
 client = Main()
-client.run(TOKEN)
+client.run(token)
